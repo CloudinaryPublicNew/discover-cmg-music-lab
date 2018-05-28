49f562ad2630aa086fd869987fa1fab95b8983d4
Dan Zeitman <dvpweb@gmail.com>
2018-05-28 11:56:56 -0700

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Discover Music Hack Lab',
+  title: 'Discover Music Lab',
   description: 'Step by step gude to build a music client app.',
   themeConfig: {
     sidebar: [
-      genSidebarConfig('Lab', '/lab/', ['', 'getting-started']),
+      genSidebarConfig('Setup', '/lab/', ['', 'getting-started']),
       genSidebarConfig('Browsing', '/browsing/', [
         'route-config-and-styling', 
         'listing', 
